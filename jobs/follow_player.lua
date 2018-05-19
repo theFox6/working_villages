ecd152f584f4f4667fe940cdd471d85d86f6696f
theFox6 <the.fox6@gmx.de>
2018-05-19 15:40:13 +0200

    remove infinite loop

--- jobs/follow_player.lua
+++ jobs/follow_player.lua
@@ -2,35 +2,32 @@ working_villages.register_job("working_villages:job_folow_player", {
 	description      = "working_villages job : follower",
 	inventory_image  = "default_paper.png^memorandum_letters.png",
 	jobfunc = function(self)
-		while (true) do
-			while (not self:is_active()) do
-				coroutine.yield()
-			end
-			local position = self.object:getpos()
-			local direction = vector.new(0,0,0)
-			local player = self:get_nearest_player(10)
-			if player~=nil then
-				local player_position = player:getpos()
-				direction = vector.subtract(player_position, position)
-			end
+		while (not self:is_active()) do
+			coroutine.yield()
+		end
+		local position = self.object:getpos()
+		local direction = vector.new(0,0,0)
+		local player = self:get_nearest_player(10)
+		if player~=nil then
+			local player_position = player:getpos()
+			direction = vector.subtract(player_position, position)
+		end
 
-			local velocity = self.object:getvelocity()
-			if vector.length(direction) < 3 then
-				if velocity.x~=0 or velocity.y~=0 then
-					self:set_animation(working_villages.animation_frames.STAND)
-					self.object:setvelocity{x = 0, y = velocity.y, z = 0}
-				end
-			else
-				if velocity.x==0 and velocity.y==0 then
-					self:set_animation(working_villages.animation_frames.WALK)
-				end
-				self.object:setvelocity{x = direction.x, y = velocity.y, z = direction.z}
-				self:set_yaw_by_direction(direction)
-
-				--if villager is stoped by obstacle, the villager must jump.
-				self:handle_obstacles(true)
+		local velocity = self.object:getvelocity()
+		if vector.length(direction) < 3 then
+			if velocity.x~=0 or velocity.y~=0 then
+				self:set_animation(working_villages.animation_frames.STAND)
+				self.object:setvelocity{x = 0, y = velocity.y, z = 0}
 			end
-			coroutine.yield() -- wait for next step
+		else
+			if velocity.x==0 and velocity.y==0 then
+				self:set_animation(working_villages.animation_frames.WALK)
+			end
+			self.object:setvelocity{x = direction.x, y = velocity.y, z = direction.z}
+			self:set_yaw_by_direction(direction)
+
+			--if villager is stoped by obstacle, the villager must jump.
+			self:handle_obstacles(true)
 		end
 	end,
 })
--- states.lua
+++ states.lua
@@ -13,8 +13,14 @@ working_villages.register_state("job",{
 		if type(job.on_step)=="function" then
 			job.on_step(self, dtime)
 		elseif self.job_thread then
+			if coroutine.status(self.job_thread) == "dead" then
+				self.job_thread = coroutine.create(job.jobfunc)
+			end
 			if coroutine.status(self.job_thread) == "suspended" then
-				coroutine.resume(self.job_thread, self)
+				state, err = coroutine.resume(self.job_thread, self)
+				if state == false then
+					error("error in job_thread " .. err)
+				end
 			end
 		end
 	end
