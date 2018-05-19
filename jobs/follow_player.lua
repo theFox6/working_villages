c3e2da827eeff736f3e8783ed16a2117b87c68de
theFox6 <the.fox6@gmx.de>
2018-05-19 14:07:57 +0200

    implemented player follower using the new job format

--- jobs/follow_player.lua
+++ jobs/follow_player.lua
@@ -1,41 +1,36 @@
-local actions={}
-actions.ACCOMPANY={self_condition=function(self)
-				local player = self:get_nearest_player(10)
-				if player == nil then
-					return false
-				end
-				if vector.length(vector.subtract(player:getpos(), self.object:getpos())) < 3 then
-					return false
-				end
-				return true
-			end,
-			func = function(self)
-				local player = self:get_nearest_player(10)
-				if player == nil then
-					working_villages.func.get_back_to_searching(self)
-					return
-				end
-				local position = self.object:getpos()
+working_villages.register_job("working_villages:job_folow_player", {
+	description      = "working_villages job : follower",
+	inventory_image  = "default_paper.png^memorandum_letters.png",
+	jobfunc = function(self)
+		while (true) do
+			while (not self:is_active()) do
+				coroutine.yield()
+			end
+			local position = self.object:getpos()
+			local direction = vector.new(0,0,0)
+			local player = self:get_nearest_player(10)
+			if player~=nil then
 				local player_position = player:getpos()
-				local direction = vector.subtract(player_position, position)
-				if vector.length(direction) < 3 then
-					working_villages.func.get_back_to_searching(self)
-					return
+				direction = vector.subtract(player_position, position)
+			end
+
+			local velocity = self.object:getvelocity()
+			if vector.length(direction) < 3 then
+				if velocity.x~=0 or velocity.y~=0 then
+					self:set_animation(working_villages.animation_frames.STAND)
+					self.object:setvelocity{x = 0, y = velocity.y, z = 0}
 				end
-				local velocity = self.object:getvelocity()
+			else
+				if velocity.x==0 and velocity.y==0 then
+					self:set_animation(working_villages.animation_frames.WALK)
+				end
 				self.object:setvelocity{x = direction.x, y = velocity.y, z = direction.z}
 				self:set_yaw_by_direction(direction)
 
 				--if villager is stoped by obstacle, the villager must jump.
 				self:handle_obstacles(true)
-			end,
-			to_state = function(self)
-				self:set_animation(working_villages.animation_frames.WALK)
-			end,
-			}
-local follower_prop = {
-	night_active = true,
-	search_idle = true,
-	searching_range = {x = 10, y = 10, z = 10}
-}
-working_villages.func.villager_state_machine_job("job_folow_player","follower",actions,follower_prop)
+			end
+			coroutine.yield() -- wait for next step
+		end
+	end,
+})
