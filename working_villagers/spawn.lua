cbe98bce507e191335f091a7efa7722e2cb241da
Dr. Steven P. Crain <s.crain@computer.org>
2021-09-06 22:48:52 -0400

    Fixed syntax errors.

--- working_villagers/spawn.lua
+++ working_villagers/spawn.lua
@@ -17,20 +17,22 @@ local function spawn(pos, node, active_object_count, active_object_count_wider)
             pos1,pos2,{"groups:walkable"})) do
         local above = minetest.get_node({x=pos.x,y=pos.y+2,z=pos.z})
         local above_def = minetest.registered_nodes[above.name] 
-                if above_def and above_def.groups.walkable then
-            spawn_pos = pos
-            goto found
+        if above_def and above_def.groups.walkable then
+            local gender = {
+                "working_villages:villager_male",
+                "working_villages:villager_female",
+            }
+            local self = minetest.add_entity(
+                {x=pos.x,y=pos.y+1,z=pos.z},gender[math.random(2)]
+            )
+            local inv = self:get_inventory()
+            local job = working_villages.job_inv:remove_item(
+                "main","working_villages:job_herbcollector"
+            ) 
+            inv:set_stack("job", 1, job)
+            return
         end
     end
-    return
-
-    ::found::
-    local gender = {"working_villages:villager_male","working_villages:villager_female"}[math.random(2)]
-    local self = minetest.add_entity(spawn_pos,gender)
-    local inv = self:get_inventory()
-    local job = working_villages.job_inv:remove_item(
-        "main","working_villages:job_herbcollector") 
-    inv:set_stack("job", 1, job)
 end
 
 minetest.register_abm({
