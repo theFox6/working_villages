13684d23aeefd6c5d3baeb49ae80e7549d43ddef
Dr. Steven P. Crain <s.crain@computer.org>
2021-10-11 18:27:59 -0400

    Usernames containing ':' are already blocked by Minetest.

--- working_villagers/jobs/util.lua
+++ working_villagers/jobs/util.lua
@@ -204,18 +204,8 @@ else
         return minetest.is_protected(pos, myowner)
     end
 
-    -- Prevent player names like "[owner_protection]:[owner_name]"
-    local prefixlen = #owner_griefing
-    local function on_prejoinplayer(name, ip)
-        if name[prefixlen + 1] == ":"
-                and name[prefixlen + 2]
-                and string.sub(name,1,prefixlen) == owner_griefing then
-            return "Your player name is reserved."
-        end
-    end
-    minetest.register_on_prejoinplayer(on_prejoinplayer)
-
     -- Patch areas to support this extension
+    local prefixlen = #owner_griefing
     if minetest.global_exists("areas") then
         local areas_player_exists = areas.player_exists
         function areas.player_exists(area, name)
