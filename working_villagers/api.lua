73c03fd20eae994030a33b751fe09ecc2b30e863
SFENCE <sfence.software@gmail.com>
2022-04-25 04:30:51 +0200

    Fix bug.

--- working_villagers/api.lua
+++ working_villagers/api.lua
@@ -273,8 +273,8 @@ function working_villages.villager:move_main_to_wield(pred)
     if pred(stack:get_name()) then
       local wield_stack = inv:get_stack("wield_item", 1)
       inv:set_stack("wield_item", 1, stack)
-      inv:take_stack("main", stack)
-      inv:add_stack("main", wield_stack)
+      inv:remove_item("main", stack)
+      inv:add_item("main", wield_stack)
       return true
     end
   end
