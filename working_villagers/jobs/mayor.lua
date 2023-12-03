0366fc9b6cce4e77fcc0182c6f5d6ff936bd9d8c
jichi <Jichi.Guo@gmail.com>
2023-12-02 18:34:56 -0600

    fixed oversight

--- working_villagers/jobs/mayor.lua
+++ working_villagers/jobs/mayor.lua
@@ -1,9 +1,36 @@
 local follower = working_villages.require("jobs/follow_player")
+local mayor    = {}
 
 local follower_tools = {
 	-- need a 3d armor light for the villagers
 	["working_villages:commanding_sceptre"] = 1,
 }
+
+function mayor.mayor_v1(v)
+	self:handle_night()
+	self:handle_chest(take_func, put_func)
+	local stack  = self:get_wield_item_stack()
+	if stack:is_empty() then
+	self:move_main_to_wield(function(name)
+  		return ruling_demands[name] ~= nil
+	end)
+	end
+	self:handle_job_pos()
+
+	local pos = self.object:get_pos()
+	minetest.forceload_block(pos, true) -- TODO unload the block if we leave it
+
+	self:count_timer("mayor:search")
+	self:count_timer("mayor:change_dir")
+	self:handle_obstacles()
+	if self:timer_exceeded("mayor:search",20) then
+		-- TODO something
+	elseif self:timer_exceeded("mayor:change_dir",50) then
+		-- TODO don't leave the village
+		--self:change_direction_randomly()
+	end
+end
+
 working_villages.register_job("working_villages:job_mayor", {
   description      = "mayor (working_villages)",
   long_description = "I keep this place running in the absence of players.",
@@ -16,7 +43,8 @@ working_villages.register_job("working_villages:job_mayor", {
     	end)
     end
     while (v.pause) do
-      coroutine.yield()
+      --coroutine.yield()
+      mayor.mayor_v1(v)
     end
     follower.step(v)
   end,
